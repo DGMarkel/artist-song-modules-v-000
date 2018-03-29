04bba307d116ff3823cdc14da03564e04b5a7c1a
David Markel <davidgmarkel@gmail.com>
2018-03-29 15:36:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,13 +15,14 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
+=end
 
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
-=end
+
 
   def self.all
     @@artists
